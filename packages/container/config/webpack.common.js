b1032cb2e15bfa8d4c1cea5b6dd2e57d6c8038d8
Liad Bela <liad.bela@genesys.com>
2022-04-19 09:49:00 +0300

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtnlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [
@@ -14,4 +16,9 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtnlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
 }
